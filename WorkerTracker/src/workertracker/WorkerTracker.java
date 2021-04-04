4e88c2a5c1c721336da94a3274bf2e2957666a7b
batuhansusam <batuhanssm@gmail.com>
2021-04-04 23:55:34 +0300

    asd

--- WorkerTracker/src/workertracker/WorkerTracker.java
+++ WorkerTracker/src/workertracker/WorkerTracker.java
@@ -15,7 +15,7 @@ public class WorkerTracker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("test");
+        System.out.print("mrb");
     }
     
 }
